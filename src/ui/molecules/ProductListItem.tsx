7111a16791325bcdf8f94f918911c3fd5c152573
KamilFil <kamilfprog@gmail.com>
2023-09-10 09:26:34 +0200

    Fix test problem

--- src/ui/molecules/ProductListItem.tsx
+++ src/ui/molecules/ProductListItem.tsx
@@ -10,7 +10,7 @@ type ProductListItemDescProps = {
 
 export const ProductListItem = ({product}: ProductListItemDescProps) => {
 return (
-    <li data-testid="products-list">
+    <li>
         <article>
             <ProductCoverImage src={product.coverImage.src} alt={product.coverImage.alt}/>
             <ProductListItemDesc product={product}/> 
